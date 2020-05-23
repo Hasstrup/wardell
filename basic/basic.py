16908f013f5867b969852acc23ab507441e12bcf
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2020-05-23 12:40:26 +0100

    [chore] user input excercises

---
+++ basic/basic.py
@@ -0,0 +1,10 @@
+
+class Basic:
+    def user_input_integers:
+        """
+        Take in two numbers from user input and calculate the average
+        """
+        number_1 = int(input("Enter first number"))
+        number_2 = int(input("Enter second number"))
+        avg = (number_1 + num_2) / 2
+        print(f"The average is {avg}")
---
+++ basic/basic_test.py
@@ -0,0 +1,11 @@
+import unittest
+from . import Basic
+
+
+class TestBasic(unittest.TestCase):
+    def test_strings(self):
+        True
+
+
+if __name__ == '__main__':
+    unittest.main()
