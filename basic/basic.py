8dd62dba17719efcbd3d8e04dfb113953968fd22
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2020-05-23 12:44:46 +0100

    user-input integers

--- basic/basic.py
+++ basic/basic.py
@@ -1,10 +1,17 @@
 
 class Basic:
-    def user_input_integers:
+    def user_input_integers(self):
         """
         Take in two numbers from user input and calculate the average
         """
-        number_1 = int(input("Enter first number"))
-        number_2 = int(input("Enter second number"))
-        avg = (number_1 + num_2) / 2
+        num_1 = int(input("Enter first number: "))
+        num_2 = int(input("Enter second number: "))
+        avg = (num_1 + num_2) / 2
         print(f"The average is {avg}")
+
+    def run_excercises(self):
+        self.user_input_integers()
+
+
+if __name__ == "__main__":
+    Basic().run_excercises()
